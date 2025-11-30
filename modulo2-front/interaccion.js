03f1bdddcfd0644b2878f600e346c7246e9c3990
Paulina Huentel <paulina.huentel@gmail.com>
2025-11-30 13:49:01 -0300

    Agregar módulo 2 - Proyecto front

---
+++ modulo2-front/interaccion.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".card").hover(
+    function () {
+      $(this).addClass("shadow-lg");
+    },
+    function () {
+      $(this).removeClass("shadow-lg");
+    }
+  );
+});
---
+++ modulo2-front/script.js
@@ -0,0 +1,8 @@
+function mostrarCV() {
+  const cv = document.getElementById("cv");
+  if (cv.style.display === "none") {
+    cv.style.display = "block";
+  } else {
+    cv.style.display = "none";
+  }
+}
